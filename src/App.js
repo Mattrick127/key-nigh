21723306547b4766cedfc0ce7b71d160a676cdec
Matt127 <mattrick2214@gmail.com>
2022-06-20 22:39:30 -0500

    wrote in a footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import About from './components/About';
+import Footer from './components/Footer';
 
 function App() {
   return (
+    <main>
     <div className="App">
       <About></About>
     </div>
+    <footer>
+      <Footer></Footer>
+    </footer>
+    </main>
   );
 }
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer id="footer">
+            If you find yourself bamboozled over Kenai's boo-tay, check back for more and more and more pictures!!!
+        </footer>
+    )
+}
+export default Footer;
