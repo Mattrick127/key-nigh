1000fe8f62b09384b5613e198f797bc18ffbfab0
Matt127 <mattrick2214@gmail.com>
2022-08-16 22:51:45 -0500

    adding in routes to photos and css.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import coverImage from '../../assets/cover/bowhat2.jpg';
 function About() {
     return (
         <section>
             <p>Kenai yo!</p>
+            <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
         </section>
-    )
+    );
 }
 
 export default About;
